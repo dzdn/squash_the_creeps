c238b27ad5ba9ece97334ac6bc19b14c4a09750c
dzdn <jdustinwilliams@gmail.com>
2023-01-10 19:11:20 -0500

    Set up player, ground and camera

---
+++ Player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody
+
+# How fast player moves in m/s
+export var speed = 14
+# Downward acceleration in m/s^2
+export var fall_acceleration = 74
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+	
+	# Ground velocity
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	# Vertical velocity
+	velocity.y -= fall_acceleration * delta
+	# Moving the character
+	velocity = move_and_slide(velocity, Vector3.UP)
